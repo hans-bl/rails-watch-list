c7960dd74617a51f5352fbaf4d5a5da7d338a65c
hans bousquet lafond <hans.bl@icloud.com>
2022-07-12 21:29:14 +0100

    App is OK

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,21 +1,33 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
   def new
+    # @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.save
-    redirect_to bookmark_path(@bookmark)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new
+    end
   end
 
   def destroy
     Bookmark.find(params[:id]).delete
+    redirect_back fallback_location: lists_path # check the method to refresh (and not redirect)
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
